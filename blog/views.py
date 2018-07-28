c29277dc6fed00deb901bc327a522bf24158326d
Leoyao87820984 <1343792156@qq.com>
2018-07-28 18:47:17 +0200

    fenye

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def index(request):
@@ -14,19 +15,51 @@ def post_list(request):
 	return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_list_ds(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list_ds.html', {'posts':posts})
+	posts_list = Post.objects.filter(published_date__lte=timezone.now(), type='ds').order_by('published_date')
+	paginator = Paginator(posts_list, 5)
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
+	return render(request, 'blog/post_list_ds.html', {'page': page, 'posts':posts})
 
 def post_list_pd(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts_list = Post.objects.filter(published_date__lte=timezone.now(), type='pd').order_by('published_date')
+	paginator = Paginator(posts_list, 5)
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
 	return render(request, 'blog/post_list_pd.html', {'posts':posts})
 
 def post_list_kg(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts_list = Post.objects.filter(published_date__lte=timezone.now(), type='kg').order_by('published_date')
+	paginator = Paginator(posts_list, 5)
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
 	return render(request, 'blog/post_list_kg.html', {'posts':posts})
 
 def post_list_os(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts_list = Post.objects.filter(published_date__lte=timezone.now(), type='os').order_by('published_date')
+	paginator = Paginator(posts_list, 5)
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
 	return render(request, 'blog/post_list_os.html', {'posts':posts})
 
 def post_detail(request, pk):
